11690f3597582c84e1cbba367f1071cb8bf34d5e
ivan <ivan@stanga.net>
2023-11-08 11:13:31 +0200

    creating IngredientsBox on hover in searched items - to be finished

---
+++ src/components/IngredientsBox.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function IngredientsBox() {
+    return <div></div>;
+}
+
+export default IngredientsBox;
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -3,57 +3,86 @@ import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Favourite from "../components/Favourite";
+// import IngredientsBox from "../components/IngredientsBox";
 function Searched() {
-  const [searchedRecipes, setSearchedRecipes] = useState([]);
-  let params = useParams();
-  console.log(params);
-  const getSearched = async (name) => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`);
-    const recipes = await data.json();
-    setSearchedRecipes(recipes.results);
-  };
+    const [searchedRecipes, setSearchedRecipes] = useState([]);
+    const [hoveredRecipe, setHoveredRecipe] = useState(null);
+    let params = useParams();
+    console.log(params);
+    const getSearched = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`);
+        const recipes = await data.json();
+        console.log(recipes);
+        setSearchedRecipes(recipes.results);
+    };
 
-  useEffect(() => {
-    getSearched(params.search);
-    console.log(params.search);
-  }, [params.search]);
+    const ingredientsBox = async (id) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=${process.env.REACT_APP_API_KEY}`);
+        const recipeIngredients = await data.json();
+        setHoveredRecipe({ id, ingredients: recipeIngredients });
+    };
 
-  return (
-    <Grid>
-      {searchedRecipes.map((recipe) => {
-        return (
-          <Card key={recipe.id}>
-            <Favourite recipe={recipe} />
-            <Link to={"/recipe/" + recipe.id}>
-              <img src={recipe.image} alt={recipe.title} />
-              <p>{recipe.title}</p>
-            </Link>
-          </Card>
-        );
-      })}
-    </Grid>
-  );
+    const removeIngredientsBox = () => {
+        setHoveredRecipe(null);
+    };
+
+    useEffect(() => {
+        getSearched(params.search);
+        console.log(params.search);
+    }, [params.search]);
+
+    return (
+        <Grid>
+            {searchedRecipes.map((recipe) => {
+                return (
+                    <Card key={recipe.id} onMouseEnter={ingredientsBox(recipe.id)} onMouseLeave={removeIngredientsBox}>
+                        <Favourite recipe={recipe} />
+                        <Link to={"/recipe/" + recipe.id}>
+                            <img src={recipe.image} alt={recipe.title} />
+                            <p>{recipe.title}</p>
+                        </Link>
+                        {hoveredRecipe && hoveredRecipe.id === recipe.id && <IngredientsBox>{}</IngredientsBox>}
+                    </Card>
+                );
+            })}
+        </Grid>
+    );
 }
 
 const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
-  grid-gap: 3rem;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+    grid-gap: 3rem;
 `;
+
 const Card = styled.div`
-  position: relative;
-  img {
+    position: relative;
+    img {
+        width: 100%;
+        height: 100%;
+        border-radius: 3rem;
+    }
+    a {
+        text-decoration: none;
+    }
+    p {
+        text-align: center;
+        padding: 1rem;
+    }
+`;
+
+const IngredientsBox = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
-    border-radius: 3rem;
-  }
-  a {
-    text-decoration: none;
-  }
-  h4 {
+    background-color: rgba(255, 255, 255, 0.9);
+    display: flex;
+    align-items: center;
+    justify-content: center;
     text-align: center;
-    padding: 1rem;
-  }
+    border-radius: 3rem;
 `;
 
 export default Searched;
